7d165888e2cd65fa8d8fcbb1ce3403c2c5b60925
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-17 12:12:08 -0600

    added jquery character counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,15 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
+  // sets up
+  $(".new-tweet-submit").on('click', function(e) {
+    alert(JSON.stringify(this));
+    //$(this).css('color', 'red');
+    e.preventDefault();
+  });
 
+  // Updates the counter
+  $(".new-tweet-text").on('keyup', function(e) {
+    $(this).siblings('span').html(140 - $(this).val().length);
+  });
+
+
+});
