2a4a29b5a84aa8c85177cf5c297a198fd6a5c2ae
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-17 23:43:05 -0600

    refactored; added jQuery ajax submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
-  // sets up
-  $(".new-tweet-submit").on('click', function(e) {
-    //alert(JSON.stringify(this));
-    //$(this).css('color', 'red');
+  // jQuery - ajax tweet submission
+  $(".new-tweet form").submit(function(e) {
     e.preventDefault();
+    $.ajax({
+      url: e.target.action,
+      method: e.target.method,
+      data: $(this).serialize()
+    }).done((e) => {
+      console.log('Tweeted successfuly');
+    }).fail((e) => {
+      console.log('Faild to Tweet');
+    });
   });
 
   // Updates the counter
-  $(".new-tweet-text").on('keyup', function(e) {
+  $(".new-tweet textarea").on('keyup', function(e) {
     $(this).siblings('span').html(140 - $(this).val().length);
   });
 
+  const createTweetElement = (tweet) => {
+    const element = `
+      <article class="tweet">
+        <header class="Clearfix">
+          <img src="${tweet.user.avatars}"/>
+          <div>${tweet.user.name}</div>
+          <div>${tweet.user.handle}</div>
+        </header>
+        <div class="tweet-text">${tweet.content.text}</div>
+        <footer class="Clearfix">
+          <div>${new Date(tweet.created_at)}</div>
+          <div>like</div>
+        </footer>
+      </article>`;
+    return element;
+  };
 
 });
