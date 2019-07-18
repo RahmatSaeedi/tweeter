3752cf898b4ba56ae3b49c3b0163ef711b12b096
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-18 13:21:45 -0600

    refactored; added form validation & tweet refetch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
   // Creates a new tweet element
-  const createTweetElement = (tweet) => {
-    const element = `
+  const createTweetElement = (tweet, includeArticleTag = false) => {
+    if (!includeArticleTag) {
+      return `
+        <header class="Clearfix">
+          <img src="${tweet.user.avatars}"/>
+          <div>${tweet.user.name}</div>
+          <div>${tweet.user.handle}</div>
+        </header>
+        <div class="tweet-text">${tweet.content.text}</div>
+        <footer class="Clearfix">
+          <div>${new Date(tweet.created_at)}</div>
+          <div>like</div>
+        </footer>`;
+    } else {
+      return `
       <article class="tweet">
         <header class="Clearfix">
           <img src="${tweet.user.avatars}"/>
@@ -14,26 +27,52 @@ $(document).ready(function() {
           <div>like</div>
         </footer>
       </article>`;
-    return element;
+    }
+  };
+
+  // Form validation:
+  const validateForm = function(form) {
+    const tweetLength = $(form).find('textarea')[0].textLength;
+    if (tweetLength > 0) {
+      if (tweetLength < 141) {
+        return true;
+      } else {
+        setTimeout(function() {
+          alert('Too many characters are in your tweet!');
+        },1);
+        return false;
+      }
+    } else {
+      setTimeout(function() {
+        alert('Empty Tweet!');
+      }, 1);
+      return false;
+    }
   };
 
   // jQuery - ajax tweet submission
   $(".new-tweet form").submit(function(e) {
     e.preventDefault();
-    if ($(this).serialize().length > 5) {
+    
+    if (validateForm(this)) {
       $.ajax({
         url: e.target.action,
         method: 'POST',
-        data: $(this).serialize()
+        port: 80,
+        data: $(this).serialize(),
+        complete: null,   // Callback when finished
+        error: null,      // Callback in case of error
+        success: () => {  // Callbackin case of success response
+          document.getElementById('tweets-container').innerHTML = '';
+          loadTweets();
+        },
+        timeout: null    // Timeout before emitting fail
       }).done((e) => {
         console.log('Tweeted successfuly');
       }).fail((e) => {
         console.log('Faild to Tweet');
       });
-    } else {
-      console.log('Empty tweet!');
     }
-
   });
 
 
@@ -44,11 +83,13 @@ $(document).ready(function() {
       method: 'GET'
     }).done((tweets) => {
       tweets.forEach(function(tweet) {
-        console.log(tweet);
-        $('#tweets-container').prepend(createTweetElement(tweet));
+        console.log(typeof(createTweetElement(tweet)));
+        const article = document.createElement('article');
+        article.classList.add('tweet');
+        article.innerHTML = createTweetElement(tweet).trim();
+        document.getElementById('tweets-container').prepend(article);
       });
-      console.log('Obtained successfuly');
-      //console.log(tweets);
+      console.log('Obtained tweets successfuly');
     }).fail((error) => {
       console.log('Faild to get tweets');
     });
@@ -61,4 +102,5 @@ $(document).ready(function() {
 
   // Loads the initial tweets
   loadTweets();
+
 });
