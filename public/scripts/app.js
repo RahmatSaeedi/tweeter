c7a805dddce4e94cc6ac2760873b973b2b2154e9
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-18 00:46:11 -0600

    refactored; added jQuery ajax tweet fetching

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
-  // jQuery - ajax tweet submission
-  $(".new-tweet form").submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: e.target.action,
-      method: e.target.method,
-      data: $(this).serialize()
-    }).done((e) => {
-      console.log('Tweeted successfuly');
-    }).fail((e) => {
-      console.log('Faild to Tweet');
-    });
-  });
-
-  // Updates the counter
-  $(".new-tweet textarea").on('keyup', function(e) {
-    $(this).siblings('span').html(140 - $(this).val().length);
-  });
-
+  // Creates a new tweet element
   const createTweetElement = (tweet) => {
     const element = `
       <article class="tweet">
@@ -35,4 +17,48 @@ $(document).ready(function() {
     return element;
   };
 
+  // jQuery - ajax tweet submission
+  $(".new-tweet form").submit(function(e) {
+    e.preventDefault();
+    if ($(this).serialize().length > 5) {
+      $.ajax({
+        url: e.target.action,
+        method: 'POST',
+        data: $(this).serialize()
+      }).done((e) => {
+        console.log('Tweeted successfuly');
+      }).fail((e) => {
+        console.log('Faild to Tweet');
+      });
+    } else {
+      console.log('Empty tweet!');
+    }
+
+  });
+
+
+  // Fetches tweets via jQuery - ajax
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets/',
+      method: 'GET'
+    }).done((tweets) => {
+      tweets.forEach(function(tweet) {
+        console.log(tweet);
+        $('#tweets-container').prepend(createTweetElement(tweet));
+      });
+      console.log('Obtained successfuly');
+      //console.log(tweets);
+    }).fail((error) => {
+      console.log('Faild to get tweets');
+    });
+  };
+
+  // Updates the character counter for new-tweet
+  $(".new-tweet textarea").on('keyup', function(e) {
+    $(this).siblings('span').html(140 - $(this).val().length);
+  });
+
+  // Loads the initial tweets
+  loadTweets();
 });
